1de4c66d4cf20b09f45996a83b92c7b788537f7f
Makoto Kurihara <makoto-kurihara@pitolick.com>
2024-04-16 23:12:53 +0900

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./src/**/*.{html,js,ts,svelte}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        'black': '#333',
+        'primary': {
+          DEFAULT: '#2CB696',
+          dark: '#008990',
+        },
+      },
+      fontFamily: {
+        noto: ['Arial','Noto Sans JP', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
+
