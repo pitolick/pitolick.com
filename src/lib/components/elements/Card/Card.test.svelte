825321cd09a26876ade85644938ddf1cd801e38f
Makoto Kurihara <makoto-kurihara@pitolick.com>
2024-04-24 01:02:47 +0900

    add Card Test

---
+++ src/lib/components/elements/Card/Card.test.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Card from './Card.svelte';
+</script>
+
+<Card
+	img={{
+		src: 'https://via.placeholder.com/150',
+		alt: 'Placeholder Image',
+		width: 150,
+		height: 150
+	}}
+	title="Card Title"><h1>slot<span data-testid="child" /></h1></Card
+>
---
+++ src/lib/components/elements/Card/Card.test.ts
@@ -0,0 +1,64 @@
+/**
+ * Cardコンポーネントのテスト
+ */
+import '@testing-library/svelte/vitest';
+import '@testing-library/jest-dom/vitest';
+
+import { describe, expect, test } from 'vitest';
+import { render, screen, within } from '@testing-library/svelte';
+import Card from './Card.svelte';
+import CardTest from './Card.test.svelte';
+
+describe('Cardコンポーネント', () => {
+	test('Cardコンポーネントのslotが正しく動作すること', () => {
+		render(CardTest, {
+			img: {
+				src: 'https://via.placeholder.com/150',
+				alt: 'Placeholder Image',
+				width: 150,
+				height: 150
+			},
+			title: 'Card Title'
+		});
+
+		const card = screen.getByRole('heading', { name: 'slot' });
+		const child = within(card).getByTestId('child');
+
+		expect(child).toBeInTheDocument();
+	});
+
+	test('Cardコンポーネントのtitle属性が正しく動作すること', () => {
+		render(Card, {
+			img: {
+				src: 'https://via.placeholder.com/150',
+				alt: 'Placeholder Image',
+				width: 150,
+				height: 150
+			},
+			title: 'Card Title'
+		});
+
+		const card = screen.getByRole('heading', { name: 'Card Title' });
+
+		expect(card).toHaveTextContent('Card Title');
+	});
+
+	test('Cardコンポーネントのimg属性が正しく動作すること', () => {
+		render(Card, {
+			img: {
+				src: 'https://via.placeholder.com/150',
+				alt: 'Placeholder Image',
+				width: 150,
+				height: 150
+			},
+			title: 'Card Title'
+		});
+
+		const img = screen.getByRole('img', { name: 'Placeholder Image' });
+
+		expect(img).toHaveAttribute('src', 'https://via.placeholder.com/150');
+		expect(img).toHaveAttribute('alt', 'Placeholder Image');
+		expect(img).toHaveAttribute('width', '150');
+		expect(img).toHaveAttribute('height', '150');
+	});
+});
