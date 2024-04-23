7d67e878f584e1abe8fc1104f6db75acce72322c
Makoto Kurihara <makoto-kurihara@pitolick.com>
2024-04-24 00:34:43 +0900

    mod stories

---
+++ src/lib/components/elements/Heading/Heading.stories.svelte
@@ -0,0 +1,51 @@
+<script context="module" lang="ts">
+	import Heading from './Heading.svelte';
+
+	export const meta = {
+		title: 'Components/Heading',
+		component: Heading,
+		tags: ['autodocs'],
+		argTypes: {
+			tag: {
+				control: { type: 'select' },
+				options: ['h2', 'h3', 'h4', 'h5', 'h6']
+			}
+		}
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+
+	let slot: string = 'This is a Heading component';
+</script>
+
+<Template let:args>
+	<Heading {...args}>
+		{slot}
+	</Heading>
+</Template>
+
+<Story name="Default">
+	<Heading>slot text</Heading>
+</Story>
+
+<Story name="h2">
+	<Heading tag="h2">h2 text</Heading>
+</Story>
+
+<Story name="h3">
+	<Heading tag="h3">h3 text</Heading>
+</Story>
+
+<Story name="h4">
+	<Heading tag="h4">h4 text</Heading>
+</Story>
+
+<Story name="h5">
+	<Heading tag="h5">h5 text</Heading>
+</Story>
+
+<Story name="h6">
+	<Heading tag="h6">h6 text</Heading>
+</Story>
---
+++ src/lib/components/elements/LinkButton/LinkButton.stories.svelte
@@ -0,0 +1,76 @@
+<script context="module" lang="ts">
+	import LinkButton from './LinkButton.svelte';
+
+	export const meta = {
+		title: 'Components/LinkButton',
+		component: LinkButton,
+		tags: ['autodocs'],
+		argTypes: {
+			targetLink: {
+				control: { type: 'select' },
+				options: ['', '_self', '_blank', '_parent', '_top']
+			}
+		}
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<LinkButton {...args} />
+</Template>
+
+<Story
+	name="Default"
+	args={{
+		text: 'LinkButton',
+		href: '#'
+	}}
+/>
+
+<Story
+	name="TargetBlank"
+	args={{
+		text: 'LinkButton',
+		href: 'https://google.com',
+		targetLink: '_blank'
+	}}
+/>
+
+<Story
+	name="TargetSelf"
+	args={{
+		text: 'LinkButton',
+		href: 'https://google.com',
+		targetLink: '_self'
+	}}
+/>
+
+<Story
+	name="TargetParent"
+	args={{
+		text: 'LinkButton',
+		href: 'https://google.com',
+		targetLink: '_parent'
+	}}
+/>
+
+<Story
+	name="TargetTop"
+	args={{
+		text: 'LinkButton',
+		href: 'https://google.com',
+		targetLink: '_top'
+	}}
+/>
+
+<Story
+	name="RelNoopener"
+	args={{
+		text: 'LinkButton',
+		href: '#',
+		rel: 'noopener'
+	}}
+/>
