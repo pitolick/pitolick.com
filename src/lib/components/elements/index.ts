27c7fbf7308804a01a56706f86fc5f65fede680f
Makoto Kurihara <makoto-kurihara@pitolick.com>
2024-04-24 13:36:25 +0900

    add endpoint

--- src/lib/components/elements/index.ts
+++ src/lib/components/elements/index.ts
@@ -2,3 +2,5 @@
 // place files you want to import through the `$lib/components/elements` alias in this folder.
 export { default as Heading } from './Heading/Heading.svelte';
 export { default as LinkButton } from './LinkButton/LinkButton.svelte';
+export { default as Card } from './Card/Card.svelte';
+export { default as SiteCard } from './SiteCard/SiteCard.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { Heading } from '$lib/components/elements';
 </script>
 
-<h1 class="text-5xl text-black after:">Welcome to SvelteKit</h1>
+<h1 class="after: text-5xl text-black">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <Heading tag="h3">This is a<br /> custom element</Heading>
